045865812e82422c06553eb543e6d9c8feed36c8
goldirana <goldirana3210@gmail.com>
2025-01-13 16:33:33 +0530

    added log config

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,28 @@
-from pathlib import Path
+import logging
+import os
 
-CONFIG_FILE_PATH = str(Path("config/config.yaml"))
-PARAMS_FILE_PATH = str(Path("params.yaml"))
+
+log_dir = "logs"
+log_file = "running.logs"
+
+# create log dir
+os.makedirs(log_dir, exist_ok=True)
+
+# define handlers
+stream_handler = logging.StreamHandler()
+file_handler = logging.FileHandler(os.path.join(log_dir, log_file))
+
+formatter = "%(asctime)s - %(module)s - %(levelname)s - %(message)s" 
+
+# Create log config
+logging.basicConfig(
+    level=logging.DEBUG,
+    format=formatter,
+    handlers=[stream_handler, file_handler]
+)
+
+logger = logging.getLogger()
+
+if __name__ == "__main__":
+    logger.info("\033[1;31m This is test\033[0m") # highlighting it with color code
+    logger.error("This is error")
---
+++ src/constants/__init__.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH = str(Path("config/config.yaml"))
+PARAMS_FILE_PATH = str(Path("params.yaml"))
